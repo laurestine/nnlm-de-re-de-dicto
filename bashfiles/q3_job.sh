7d6d034eeac690227eeb17c2cc897ad3f167ad2a
Gaurav Kamath <gauravkamath1@gmail.com>
2023-06-14 20:25:53 -0400

    remove extra scripts

--- bashfiles/q3_job.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-module load python/3.8
-cd drddprojenv
-source $HOME/drddprojenv/bin/activate
-python -m spacy download en_core_web_lg
-export quarter=q3
-python 'get_roberta_scores.py'
